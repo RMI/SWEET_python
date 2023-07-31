a0dd4c21c738274a75bcd71a9b9fb7691de8923d
HughRunyan <hugh.runyan@gmail.com>
2023-07-31 14:17:05 -0700

    updates

---
+++ city_manual_baselines.py
@@ -0,0 +1,25 @@
+
+
+
+manual_baselines = {}
+
+manual_baselines['Alexandria'] = {
+    'divs': {
+        'compost': {
+            'food': 0
+            'green': 0
+            'wood': 0
+            'paper_cardboard': 0
+            }
+        'anaerobic':    
+        'combustion':    
+        'recycling':
+        }
+    'div_component_fractions': {
+        'compost':
+        'anaerobic':    
+        'combustion':    
+        'recycling':
+        }
+    
+    }
