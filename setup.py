195c4633dfb87a9e2250fdfb48cd0ea398c363ed
HughRunyan <HughRunyan@users.noreply.github.com>
2023-09-07 13:06:43 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        numpy
-        pandas
-        pycountry
-        scipy
-        matplotlib
+        "numpy"
+        "pandas"
+        "pycountry"
+        "scipy"
+        "matplotlib"
     ],
 )
